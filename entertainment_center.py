2b91308248a3089b85a4a2f91306e9e28b39c1a7
Renan Machado <renangms@gmail.com>
2018-07-06 09:31:22 -0500

    required code review changes

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,20 +1,31 @@
+"""Main module responsivle for:
+       - movie database
+       - creating web page
+"""
+
 import media
 import fresh_tomatoes
 
-godfather = media.Movie("The Godfather",
-                        "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
-                        "https://upload.wikimedia.org/wikipedia/en/1/1c/Godfather_ver1.jpg",
-                        "https://www.youtube.com/watch?v=dNE2PvbesQU")
+godfather = media.Movie(
+    "The Godfather",
+    "The aging patriarch of an organized crime dynasty "
+    "transfers control of his clandestine empire to his reluctant son.",
+    "https://upload.wikimedia.org/wikipedia/en/1/1c/Godfather_ver1.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=dNE2PvbesQU")
 
-scarface = media.Movie("Scarface",
-                       "In Miami in 1980, a determined Cuban immigrant takes over a drug cartel and succumbs to greed.",
-                       "https://upload.wikimedia.org/wikipedia/en/thumb/7/71/Scarface_-_1983_film.jpg/220px-Scarface_-_1983_film.jpg",
-                       "https://www.youtube.com/watch?v=cv276Wg3e7I")
+scarface = media.Movie(
+    "Scarface",
+    "In Miami in 1980, a determined Cuban immigrant takes "
+    "over a drug cartel and succumbs to greed.",
+    "https://upload.wikimedia.org/wikipedia/en/thumb/7/71/Scarface_-_1983_film.jpg/220px-Scarface_-_1983_film.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=cv276Wg3e7I")
 
-goodfellas = media.Movie("Goodfellas",
-                         "The story of Henry Hill and his life in the Italian-American crime syndicate.",
-                         "https://upload.wikimedia.org/wikipedia/en/7/7b/Goodfellas.jpg",
-                         "https://www.youtube.com/watch?v=xWMxvFvhAB8")
+goodfellas = media.Movie(
+    "Goodfellas",
+    "The story of Henry Hill and his life in "
+    "the Italian-American crime syndicate.",
+    "https://upload.wikimedia.org/wikipedia/en/7/7b/Goodfellas.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=xWMxvFvhAB8")
 
 if __name__ == "__main__":
     movies = [godfather, scarface, goodfellas]
--- media.py
+++ media.py
@@ -2,11 +2,13 @@
 
 import webbrowser
 
+
 class Movie():
     """This class provides a way to store movie information."""
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
-    def __init__(self, title, storyline, poster_image_url, trailer_youtube_url):
+    def __init__(self, title, storyline,
+                 poster_image_url, trailer_youtube_url):
         self.title = title
         self.storyline = storyline
         self.poster_image_url = poster_image_url
